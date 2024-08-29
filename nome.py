a7097c8813cfa87dcd4b9078452e0704a62a3966
safira rodrigues <safirarodrigues.souza08@gmail.com>
2024-08-29 11:49:46 -0300

    Add files via upload

---
+++ nome.py
@@ -0,0 +1,15 @@
+# atribuição de variaveis
+nome = "safira"
+idade = 16
+altura = 1.64
+
+# exibir mensagem na tela
+print("nome:", nome,"idade:", idade, "altura:", altura,)
+# formatação com f-string
+print(f"nome: {nome} idade:{idade} altura:{altura}")
+
+nome = input("digite o seu nome: ")
+idade = int (input("digite a sua idade: "))
+altura = float(input("digite a sua altura:"))
+
+print(f"nome: {nome} idade:{idade} altura:{altura}")
